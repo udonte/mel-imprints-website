7c8c04c28eed16dda306f6b135bed226e3e18fad
George Udonte <georgeudonte@gmail.com>
2023-03-17 14:37:01 +0100

    implemented features, pricing, testiomonials

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -5,7 +5,7 @@ import { overview } from '../data';
 const Brands = () => {
   const { brands, brandText } = overview;
   return <section className='py-9 flex flex-col justify-center items-center gap-4'>
-    <h2 className='ls-2 text-light text-lg'>{brandText}</h2>
+    <h2 className='text-xl text-center font-medium tracking-wider text-purple-800'>{brandText}</h2>
     <div className='container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6'>
       {/* brands */}
       {brands.map((item, index) => {
